79e29ebee5b3355b24db65f9ff3f0497ee8649e6
h4henry <97231532+h4henry@users.noreply.github.com>
2022-11-20 23:54:18 -0800

    comit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 8 - Starter
+1. Within a Github action that runs whenever code is pushed. Because we would like to use the gihub action to automaticly test the code that we pushed to the repository.
+2. No
+3. No. Because testing a message app would be involving different functions within the application. Unit test is for only one of the function.
+4. Yes. Because testing the max message length feature would only be one of the features to cover in a unit test.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,80 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// Function 1
+test('111-111-1111 is a phone number', () => {
+    expect(functions.isPhoneNumber("111-111-1111")).toBe(true);
+  });
+test('111-111-1112 is a phone number', () => {
+    expect(functions.isPhoneNumber("111-111-1112")).toBe(true);
+  });
+test('111-111-11 is not a phone number', () => {
+    expect(functions.isPhoneNumber("111-111-11")).toBe(false);
+  });
+  
+test('111-111-111 is not a phone number', () => {
+    expect(functions.isPhoneNumber("111-111-111")).toBe(false);
+  });
+
+// Function 2
+
+test('hhhh@ucsd.edu is an email', () => {
+    expect(functions.isEmail("hhhh@ucsd.edu")).toBe(true);
+  });
+test('1234@ucsd.edu is an email', () => {
+    expect(functions.isEmail("1234@ucsd.edu")).toBe(true);
+  });
+test('111-111-11 is not an email', () => {
+    expect(functions.isEmail("111-111-11")).toBe(false);
+  });
+  
+test('111-111-111 is not a an email', () => {
+    expect(functions.isEmail("111-111-111")).toBe(false);
+  });
+
+
+// Function 3
+test('hhhh@ucsd.edu is a strong password', () => {
+    expect(functions.isStrongPassword("hhhh@ucsd.edu")).toBe(true);
+  });
+test('1234@ucsd.edu is a strong password', () => {
+    expect(functions.isStrongPassword("1234@ucsd.edu")).toBe(true);
+  });
+test('1 is not a strong password', () => {
+    expect(functions.isStrongPassword("1")).toBe(false);
+  });
+test('a is not a strong password', () => {
+    expect(functions.isStrongPassword("a")).toBe(false);
+  });
+
+// Function 4
+test('1/22/2022 is a date', () => {
+    expect(functions.isDate("1/22/2022")).toBe(true);
+  });
+test('1/22/2020 is a date', () => {
+    expect(functions.isDate("1/22/2020")).toBe(true);
+  });
+test('1 is not a date', () => {
+    expect(functions.isDate("1")).toBe(false);
+  });
+test('a is not a date', () => {
+    expect(functions.isDate("a")).toBe(false);
+  });
+
+
+// Function 5
+
+
+test('123 is a hexcolor', () => {
+    expect(functions.isHexColor("123")).toBe(true);
+  });
+test('122 is a hexcolor', () => {
+    expect(functions.isHexColor("122")).toBe(true);
+  });
+test('1 is not a hexcolor', () => {
+    expect(functions.isHexColor("1")).toBe(false);
+  });
+test('a is not a hexcolor', () => {
+    expect(functions.isHexColor("a")).toBe(false);
+  });
